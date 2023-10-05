3dcf94c44ab7cf7196820b02a6d3a5a16d865060
frodorocky <frodorocky@hotmail.com>
2023-10-05 14:57:48 +1100

    init

---
+++ app/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 22:11:57 2023
+
+@author: frodo
+"""
+
+import pandas as pd
+import lightgbm as lgb
+import pickle
+import gdown  # Import gdown for file downloading
+from datetime import datetime, timedelta
+from fastapi import FastAPI, Query, HTTPException
+from starlette.responses import JSONResponse
+
+
+app = FastAPI()
+
+# load data
+url = "https://drive.google.com/uc?export=download&id=1MxzLINcFcUrdoTQZGLsBcrPDpBM0rbEL"
+output = 'predictive_test_data.csv'
+gdown.download(url, output, quiet=False)
+
+predict_df = pd.read_csv(output)
+
+#load predictive model
+predictive_model = lgb.Booster(model_file='models/lightgbm.txt')
+
+#load forecasting model
+with open('models/forecasting/holt_winters_model.pkl', 'rb') as f:
+    forecasting_model = pickle.load(f)
+
+@app.get("/")
+def root():
+    return {
+        "description": "This is a sales forecasting API with several endpoints to predict sales volume.",
+        "endpoints": {
+            "/": "Brief description of the project and endpoints.",
+            "/health/": "Check the health status of the API.",
+            "/sales/national/": "Get the next 7 days sales volume forecast for an input date.",
+            "/sales/stores/items/": "Get predicted sales volume for an input item, store, and date."
+        },
+        "Github repo": "https://github.com/user/repo"
+    }
+
+@app.get('/health', status_code=200)
+def healthcheck():
+    return 'predictive and forecasting model are all ready to go!'
+
+@app.get("/sales/national/")
+def sales_national(date: str):
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid date format. Please use 'YYYY-MM-DD'.")
+        
+    # Define the start date
+    start_date = datetime.strptime("2015-04-18", "%Y-%m-%d")
+    
+    # Calculate the difference in days
+    days_difference = (input_date - start_date).days + 7
+    
+    # Forecast using the days_difference as steps
+    results = forecasting_model.forecast(steps=days_difference)
+
+    # Extract the prediction for the next 7 days
+    forecast_next_7_days = results[-7:]
+    
+    # Mocking data for next 7 days forecast
+    forecast = {str(input_date + timedelta(days=i)): forecast_next_7_days[i] for i in range(7)}
+    
+    return JSONResponse(forecast)
+
+@app.get("/sales/stores/items/")
+def sales_stores_items(date: str, item_id: str, store_id: str):
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid date format. Please use 'YYYY-MM-DD'.")
+     
+    filtered_rows = predict_df[
+        (predict_df['date'] == date) &
+        (predict_df['item_id'] == item_id) &
+        (predict_df['store_id'] == store_id)
+    ]
+
+    if filtered_rows.empty:
+        raise HTTPException(status_code=404, detail="No matching data found for the given parameters.")
+    
+    # Mocking data for sales volume prediction
+    pred = predictive_model.predict(filtered_rows)
+    
+    return JSONResponse(pred)
+
+
